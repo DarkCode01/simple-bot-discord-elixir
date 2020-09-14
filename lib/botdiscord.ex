f9fab03fe6cb75dc5d2275181ad77b68d5851c84
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-09-13 21:47:43 -0400

    simple bot to discord

---
+++ lib/botdiscord.ex
@@ -0,0 +1,2 @@
+defmodule Botdiscord do
+end
---
+++ lib/botdiscord/application.ex
@@ -0,0 +1,18 @@
+defmodule Botdiscord.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      Botdiscord.Consumer
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Botdiscord.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
